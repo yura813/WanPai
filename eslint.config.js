7af9cdf1ea84513cf3bd18490bdab13731153f0d
pcyyura <cyp.yura@gmail.com>
2025-05-08 11:20:24 +0800

    CI test

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,30 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
-import globals from 'globals'
 import js from '@eslint/js'
+import globals from 'globals'
 import pluginVue from 'eslint-plugin-vue'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default defineConfig([
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
-  },
-
+  // 忽略資料夾（這不受 files 限制，會全域忽略）
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
   {
+    files: ['**/*.{js,mjs,cjs,vue}'],
     languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
+      globals: globals.browser,
+    },
+    plugins: {
+      vue: pluginVue,
+    },
+    extends: [
+      js.configs.recommended,
+      pluginVue.configs['flat/recommended'],
+      eslintConfigPrettier, // 最後放 Prettier 避免衝突
+    ],
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      'no-console': 'warn',
     },
   },
-
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-  skipFormatting,
 ])
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+console.log('test')
 </script>
 
 <template>
