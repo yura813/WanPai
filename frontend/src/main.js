16843372cca7d162944a68131ed55374750b84f8
Rae lin <llllyuxx@gmail.com>
2025-05-23 14:37:19 +0800

    modify the style in OrderManager.vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,8 +6,6 @@ import PrimeVue from 'primevue/config'
 import 'primeicons/primeicons.css'
 import Aura from '@primeuix/themes/aura'
 
-import 'primeicons/primeicons.css'
-
 
 const app = createApp(App);
 
